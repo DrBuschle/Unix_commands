d094983beadae3e3461e84f94a5974ea2cf2b994
DrBuschle <dr.buschle@hotmail.com>
2019-04-18 12:20:20 +0200

    updates!

---
+++ Remove_files_and_folders/rm/Remove_files_and_or_folders.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# remove files
+
+rm filename		#removes the file
+rm -rf foldername	# removes the folder with its content
+rm -rf foldername/*	# removes the content of a folder, but not the folder itself
---
+++ Rename_files/mv/Rename_files.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# rename a file
+
+mv old_name	new_name
