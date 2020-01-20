23c76b8ef389e9811acace68b426c13d6e92c0b9
DrBuschle <dr.buschle@hotmail.com>
2020-01-20 14:00:15 +0100

    Newest_updates

--- Count/Count_file_lines/wc.sh
+++ Count/Count_file_lines/wc.sh
@@ -7,8 +7,14 @@ wc -l
 
 # count the lines a files ending on a extension and writing them to a file
 
-for k in `ls *.ending`; 		# iterate with a loop through all 
+for k in `ls *.ending`; do		# iterate with a loop through all 
 					# the files with a certain ending 
-do wc -l $k >> counts.txt;		# count lines and write them to a file
+wc -l $k >> counts.txt;			# count lines and write them to a file
 done					# end loop
 
+
+# for copying
+
+for k in `ls *.fa`; do 
+wc -l $k >> counts.txt; 
+done	
