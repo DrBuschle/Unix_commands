3e60d4008860b2f11a7c21f0374815d2f12291d0
DrBuschle <dr.buschle@hotmail.com>
2020-02-05 17:28:56 +0100

    Newest_updates

--- Biology_related/Calculate_Coverage/Calculate_coverage.sh
+++ Biology_related/Calculate_Coverage/Calculate_coverage.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 # Calculate coverage after sequencing
+# Better to do this with smaller regions or chromosomes, since the files get huge
 
-samtools depth 
+
+samtools depth Your_File.bam > Coverage_of_Your_File.txt
