d85f50e9a965d2eb448104af2748388be3d7045f
DrBuschle <dr.buschle@hotmail.com>
2020-02-11 17:27:25 +0100

    Newest_updates

--- Exchange_a_pattern/gsub/Extract_a_pattern.sh
+++ Exchange_a_pattern/gsub/Extract_a_pattern.sh
@@ -2,6 +2,14 @@
 
 # rename the filenames with a certain pattern
 
+
+# PATTERN needs to exist within all files, e.g. .txt
+# PATTERN gets exchanged by NEW_PATTERN, e.g. .txt by .csv
+
+
 for name in *.ending;do
-mv "$name" "${name/NAME/RENAME}";
+mv "$name" "${name/PATTERN/NEW_PATTERN}";	
 done
+
+
+
